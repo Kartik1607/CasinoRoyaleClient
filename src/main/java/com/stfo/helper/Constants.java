30ec9058f2d5e8352b1b25aaf9d4d573c999d34b
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-11-18 15:59:07 +0530

    Random number generator

--- src/main/java/com/stfo/helper/Constants.java
+++ src/main/java/com/stfo/helper/Constants.java
@@ -1,6 +1,9 @@
 package com.stfo.helper;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Constants {
@@ -32,9 +35,146 @@ public class Constants {
 		multipliers[iEVEN] = new BigDecimal("1.25");
 	}
 	
-	private static Random rand = new Random();
 	
 	public static int getRandomNumber() {
-		return rand.nextInt(37);
+		return generateRandom(20);
 	}
+	
+
+	 public static int generateRandom(int cycles) {
+	   List<Integer> even = new ArrayList<>(); // Stores all random numbers with even digits
+	   List<Integer> odd = new ArrayList<>(); // Stores all random numbers with odd digits
+	   /*
+	    * Step 1 : Generate 20 random numbers
+	    */
+	   fillList(even, odd, cycles); // Generates the list
+	   int res = 0; //Result random number
+	   
+	   /*
+	    * Example : 
+	    * Random numbers generated are : 
+	    * EVEN [48, 81, 56] => Even-Odd = [4, -7, 1]
+	    * ODD [012, 791, 181] => Even-Odd = [-1, 1, 6]
+	    */
+	   
+	   /*
+	    * Numbers with even digits do not change polarity of the sum of other numbers.
+	    * We want to minimize odd - even for final number 
+	    * 		OD
+	    * 	maximize even - odd for final number.
+	    * 
+	    * STEP 2 : Add all positive even digit numbers to result.
+	    * 
+	    * Example :
+	    * We take [48,56] => Result = 48-56 => Even-Odd = 4+1 = 5
+	    */
+	   for(int i = 0 ; i < even.size() ; ++i) {
+	     if (even.get(i) >= 0) {
+	       res += even.get(i);
+	     }
+	   }
+	   
+	   /* STEP 3
+	    * Odd numbers change the polarity of sum of next number.
+	    * First we want to take up all the even digit numbers. 
+	    * To make all negative even digit numbers positive, we take the biggest odd number.
+	    * This effectively turns all +ve sums to -ve and vice versa.
+	    * 
+	    * Example :
+	    * We take 181 from the list
+	    * Result 48-56-181 => 4+1+6 = 11.
+	    * Now polarity of other numbers must be changed.
+	    * 
+	    * List now is 
+	    * 
+	    * EVEN [81] => Even-Odd = [+7]
+	    * ODD [012, 791] => Even-Odd = [+1, -1]
+	    */
+	   Collections.sort(odd);
+	   res += odd.get(odd.size() - 1);
+	   odd.remove(odd.size()-1);
+	   /*
+	    * STEP 4
+	    * Take remaining even digit numbers. 
+	    * At this point, even digits negative numbers are turned +ve due to previous odd number
+	    * 
+	    * Example :
+	    * We take  81
+	    * Result => 48-56-181-81 => 4 + 1 + 6 + 7 = 18
+	    */
+	   for(int i = 0 ; i < even.size() ; ++i) {
+	     if (even.get(i) < 0) {
+	       res += (even.get(i) * -1);
+	     }
+	   }
+	   
+	   boolean isPositive = false; //Checks if current sign is +ve or not. Previous odd number made it -ve.
+	   
+	   /*
+	    * STEP 5
+	    * Recursively take biggest odd number. Change polarity of sign after each iteration.
+	    * 
+	    * Example 
+	    * 
+	    * ODD [012, 791]  => Even-Odd = [+1, -1]
+	    * 
+	    * Iteration 1
+	    * Result => 48-56-181-81-012 => 4 + 1 + 6 + 7 + 1= 19
+	    * ODD [791] => Even-Odd [+1]
+	    * 
+	    * Iteration 2
+	    * Result => 48-56-181-81-012-791 => 4 + 1 + 6 + 7 + 1 + 1= 20
+	    */
+	   while (odd.size() > 0) {
+	     if (isPositive) {
+	       res += odd.get(odd.size() -1 ); //Largest odd number is at back of list
+	       odd.remove(odd.size() - 1);
+	     } else {
+	       res += (-1 * odd.get(0)); //Largest odd number is at front of list
+	       odd.remove(0);
+	     }
+	     isPositive = !isPositive;
+	   }
+	   
+	   /*
+	    * STEP 6: We found highest even-odd. But we want smallest odd-even.
+	    * We simply reverse the polarity of result.
+	    * 
+	    * Result = -20. This is minimum odd - even possible.
+	    */
+	   res = res*-1;
+	   
+	   /*
+	    * STEP 7 : Random number is res % 37.
+	    * Answer = -20 + 37 = 17
+	    */
+	   while(res < 0) {
+	     res+=37;
+	   }
+	   res = res % 37;
+	   return res;
+	 }
+	 
+	 private static void fillList(List<Integer> even, List<Integer> odd, int n) {
+	   Random rand = new Random();
+	   for(int i = 0; i < n; ++i) {
+	     int digit = rand.nextInt(10) + 1; // Random number b/w 1 -10 [number of digits]
+	     int evenSum = 0; // Stores sum of digits at even places.
+	     int oddSum = 0; // Stores sum of digits at odd places.
+	     for(int j = 1; j <= digit; ++j) {
+	       int x = rand.nextInt(10);
+	       if(j%2 == 0) {
+	         evenSum += x;
+	       } else {
+	         oddSum += x;
+	       }
+	     }
+	     if(digit % 2 == 0) {
+	       even.add(evenSum - oddSum); 
+	     } else {
+	       odd.add(evenSum - oddSum);
+	     }
+	   }
+	 }
+
 }
